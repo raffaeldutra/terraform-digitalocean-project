385dce52d61d0238b0fad18137cf93967fed0cad
Rafael Dutra <raffaeldutra@gmail.com>
2018-11-30 10:21:31 -0300

    Added variables

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  type        = string
+  description = "Name for your project at Digital Ocean"
+  default     = "Project [terraform.workspace]"
+}
+
+variable "description" {
+  type = string
+  description = "A description for this project"
+}
+
+variable "purpose" {
+  type        = string
+  description = "The purpose for your project. (example: web-app)"
+}
