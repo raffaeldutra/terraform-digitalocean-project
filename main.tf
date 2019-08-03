3e9bb15b7aabe290a65ac06bde3a0e2789eef95d
Rafael Dutra <raffaeldutra@gmail.com>
2019-08-03 17:55:04 -0300

    Fixing resources

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
+module "droplet" {
+  source = "git::https://github.com/raffaeldutra/tf-do-droplet?ref=dev"
+}
+
 resource "digitalocean_project" "project" {
   name        = "${var.name}-${terraform.workspace}"
   description = var.description
   purpose     = var.purpose
   environment = terraform.workspace
-  resources   = var.resources
+  resources   = [module.droplet.urn]
 }
