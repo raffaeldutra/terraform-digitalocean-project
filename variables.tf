f630959c7417828932fdec77c13cb7d194274c33
Rafael Dutra <raffaeldutra@gmail.com>
2019-08-07 01:22:48 -0300

    Added description for variables and now all variables are in alphabetical order

--- variables.tf
+++ variables.tf
@@ -1,15 +1,30 @@
-variable "name" {
-  type        = string
-  description = "Name for your project at Digital Ocean"
-  default     = "Project [terraform.workspace]"
-}
-
 variable "description" {
   type        = string
-  description = "A description for this project"
+  description = "(Required) A description for this project"
+}
+
+variable "droplets" {
+  type        = list
+  description = "(Optional) List with all Droplets to be part of this project"
+  default     = []
+}
+
+variable "environment" {
+  type        = map
+  description = "(optional) Edit values below for every environment name that you want"
+  default = {
+    "dev" = "Development"
+    "hom" = "Homologation"
+    "prd" = "Production"
+  }
+}
+
+variable "name" {
+  type        = string
+  description = "(Required) Name for your project at Digital Ocean"
 }
 
 variable "purpose" {
   type        = string
-  description = "The purpose for your project. (example: web-app)"
+  description = "(Required) The purpose for your project. (example: web-app)"
 }
